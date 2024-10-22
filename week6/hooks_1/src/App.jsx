29034d2b0c1f8b2a39bd8a9be68a6b8bd4937ef9
dhakarRaghu <rdhakad2002@gmail.com>
2024-10-22 16:58:29 +0530

    week 6 react_hooks

---
+++ week6/hooks_1/src/App.jsx
@@ -0,0 +1,16 @@
+function App(){
+  return (
+    <>
+        <Header title="hari"></Header>
+        <Header title="har2"></Header>
+    </>
+  )
+}
+
+function Header({title}){
+  return <div>
+    {title}
+  </div>
+}
+
+export default App
