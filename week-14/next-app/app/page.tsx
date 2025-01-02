10589f979b0d5cbd247b7293361d7485c0cd2cce
dhakarRaghu <rdhakad2002@gmail.com>
2025-01-02 17:06:49 +0530

    Backend in Next.js week-14.2

--- week-14/next-app/app/page.tsx
+++ week-14/next-app/app/page.tsx
@@ -1,9 +1,25 @@
-import Image from "next/image";
+import axios from "axios";
 
-export default function Home() {
+ function  getUserDetails() {
+  // await new Promise((resolve) => setTimeout(resolve, 3000));
+  const response = axios.get("http://localhost:3000/api/user")
+	return response.data;
+}
+
+export default async function Home() {
+  const userData = await getUserDetails();
+
   return (
-    <div >
-     hi there
+    <div className="flex flex-col justify-center h-screen">
+        <div className="flex justify-center">
+            <div className="border p-8 rounded">
+                <div>
+                    Name: {userData?.name}
+                </div>
+                
+                {userData?.email}
+            </div>
+        </div>
     </div>
   );
 }
