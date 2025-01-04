e5b218ce65277ba2c25dfe586dfbbab1d0cd7a80
dhakarRaghu <rdhakad2002@gmail.com>
2025-01-04 17:35:02 +0530

    adding different project to turborepo

---
+++ week-16/turbo/apps/backend/src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({
+    message : "hello world"
+  });
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
